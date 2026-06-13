2f2790bc37dce782c680fc2dd343a1179c487c40
HamzaGa-Dev <omar33asi@gmail.com>
2026-06-13 15:52:15 +0300

    Add files via upload

---
+++ project1.py
@@ -0,0 +1,5 @@
+#print('hello ' + input('enter your name\n')) تؤدي نفس الغرض للسطرين اللي تحت
+print('welcome to the Youtube channel name Generator:')
+name = input('what is your nickname')
+channel = input('what is your channel about?')
+print('you could name your channel ' + '(' + channel +' with ' + name +')' )
